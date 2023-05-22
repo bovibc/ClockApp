997a80bbd8daa2ed45068c9b4a800d522b223a5c
Clissia Bozzer Bovi <clissiabozzerbovi@10-40-48-39.0200.ecps.br.iosda.org>
2023-05-22 10:50:40 -0300

    Initial commit

---
+++ ClockApp/ClockApp/ClockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockAppApp.swift
+//  ClockApp
+//
+//  Created by Clissia Bozzer Bovi on 22/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ClockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
